345356a22a755e3ec5c90194a8ec00d67aff497c
AA Eftekhari <e.eftekhari@gmail.com>
2017-06-13 19:11:19 +0200

    Nikola auto commit.
    Nikola version: 7.8.7

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "AAE"  # (translatable)
+BLOG_AUTHOR = "Ali A. Eftekhari"  # (translatable)
 BLOG_TITLE = "FVTool"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link
@@ -775,7 +775,7 @@ SOCIAL_BUTTONS_CODE = ""
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+INDEX_DISPLAY_POST_COUNT = 3
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
